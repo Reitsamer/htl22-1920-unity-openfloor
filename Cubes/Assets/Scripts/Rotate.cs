3e3f46b40f78ad1d61bea94c90c1017ffe0b09de
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-11-14 15:00:24 +0100

    Session 01

---
+++ Cubes/Assets/Scripts/Rotate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 100f;
+
+    private Material material;
+
+    private Color[] colors =
+    {
+        Color.blue,
+        Color.green,
+        Color.magenta,
+        Color.red,
+        Color.yellow,
+    };
+
+    private int colorIndex = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        material = meshRenderer.sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            colorIndex = colorIndex + 1;
+            if (colorIndex >= colors.Length)
+                colorIndex = 0;
+            
+            material.color = colors[colorIndex];
+        }
+
+        transform.Rotate(Vector3.up, Time.deltaTime * speed);
+    }
+}
